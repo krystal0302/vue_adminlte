5fe494a2efb07d272c55dcd23b6c841efbc57042
Gerald.Tsai <gerald.tsai@farobottech.com>
2023-03-07 16:09:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+      
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+
+      <div class="content-wrapper">
+        
+        <ContentHeader></ContentHeader>
+
+        <section class="content">
+          <div class="container-fluid">
+          </div>
+        </section>
+        
+      </div>
+
+      <ControlSidebar></ControlSidebar>
+      
+    </div>
+  </body>
+</template>
+
+<script>
+import $ from 'jquery'
+import "../node_modules/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css"
+import "../node_modules/admin-lte/plugins/select2/css/select2.min.css"
+
+import ContentHeader from "./views/ContentHeader.vue"
+import Navbar from "./views/Navbar.vue";
+import Sidebar from "./views/Sidebar.vue";
+import ControlSidebar from "./views/ControlSidebar.vue"
+
+
+export default {
+  name: "App",
+  components: {
+    ContentHeader,
+    Navbar,
+    Sidebar,
+    ControlSidebar
+  },
+  methods: {
+    initializeSelect2() {
+      //Initialize Select2 Elements
+      $(".select2").select2();
+
+      //Initialize Select2 Elements
+      $(".select2bs4").select2({
+        theme: "bootstrap4",
+      });
+    },
+  },
+  mounted() {
+    this.initializeSelect2()
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback");
+</style>
---
+++ src/views/Sidebar.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.html" class="brand-link">
+      <img src="../assets/FARobotLogo.png" alt="FARobot Logo" class="brand-image img-circle elevation-3 logo-xs">
+      <img src="../assets/FARobotLogo_lg.png" alt="FARobot Logo" class="brand-image logo-xl">
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <!-- <div class="image">
+          <img src="../assets/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Carlos Carvalho</a>
+        </div> -->
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="pages/gallery.html" class="nav-link">
+              <i class="nav-icon far fa-image"></i>
+              <p>
+                Gallery
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/kanban.html" class="nav-link">
+              <i class="nav-icon fas fa-columns"></i>
+              <p>
+                Kanban Board
+              </p>
+            </a>
+          </li>
+          
+          
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
